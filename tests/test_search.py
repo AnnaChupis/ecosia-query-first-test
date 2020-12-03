e0d205584fa76a9f34cdb87d6921ac8663fdf7b9
AnnaChupis <rf2@ukr.net>
2020-12-03 12:19:09 +0200

    rename and empty line add

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,7 +2,7 @@ from selene.support.shared import browser
 from selene import have
 
 
-def test_title_text():
+def test_results_accuracy():
     browser.open('https://www.ecosia.org/')
 
     browser.element('[name=q]').type('yashaka selene').press_enter()
